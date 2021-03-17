6da70082fce9b6c32ec7da66b6c65d78a3b9ed2f
Nicolas Noriega <nicolasn8171@gmail.com>
2021-03-17 13:25:39 -0700

    Created MyServlet.java file

--- portfolio/src/main/java/com/google/sps/servlets/MyServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MyServlet.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/ayo")
+public class MyServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-  {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/html;");
+        response.getWriter().println("<h1>Hopefully this has been reached</h1>");
+    }
 }
