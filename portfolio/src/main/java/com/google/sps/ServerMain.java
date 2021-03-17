041762f4564912c2b16487e4835bb8b0831aa772
Nicolas Noriega <nicolasn8171@gmail.com>
2021-03-17 14:19:45 -0700

    Updated pom.xml file to reflect Jetty version change

--- portfolio/src/main/java/com/google/sps/ServerMain.java
+++ portfolio/src/main/java/com/google/sps/ServerMain.java
@@ -12,7 +12,8 @@ import org.eclipse.jetty.webapp.WebInfConfiguration;
  * Starts up the server, including a DefaultServlet that handles static files, and any servlet
  * classes annotated with the @WebServlet annotation.
  */
-public class ServerMain {
+public class ServerMain
+{
 
   public static void main(String[] args) throws Exception {
 
